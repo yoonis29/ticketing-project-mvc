6be55e3238c3c459ec3dcaa11a742537b94f1032
yoonisabdurahman <yoonis215@gmail.com>
2023-10-03 19:45:50 -0700

    modified user class

--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -1,7 +1,13 @@
 package com.cydeo.entity;
 
 import com.cydeo.enums.Gender;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Data
 public class User extends BaseEntity{
 
     private String firstName;
@@ -12,4 +18,14 @@ public class User extends BaseEntity{
     private Role role;
     private Gender gender;
 
+    public User(Long id, LocalDateTime insertDateTime, Long insertUserID, LocalDateTime lastUpdateDateTime, Long lastUpdateUserId, String firstName, String lastName, String userName, String passWord, boolean enabled, Role role, Gender gender) {
+        super(id, insertDateTime, insertUserID, lastUpdateDateTime, lastUpdateUserId);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.passWord = passWord;
+        this.enabled = enabled;
+        this.role = role;
+        this.gender = gender;
+    }
 }
