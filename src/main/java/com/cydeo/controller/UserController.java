37d77ecab7de932083d5136d7cb2e52dea847b92
yoonisabdurahman <yoonis215@gmail.com>
2023-10-13 21:48:17 -0700

    created method for delete path

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -61,5 +61,13 @@ public class UserController {
         return "redirect:/user/create";
 
     }
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username){
+
+        userService.deleteById(username);
+
+        return "redirect:/user/create";
+
+    }
 
 }
