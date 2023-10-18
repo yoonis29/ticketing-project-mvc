d838a537a3af249dd80ed595cdad3f88f427f95e
yoonisabdurahman <yoonis215@gmail.com>
2023-10-17 22:16:24 -0700

    implementing PostMaping for project endpoint

--- src/main/java/com/cydeo/controller/ProjectController.java
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -6,6 +6,7 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -27,5 +28,12 @@ public class ProjectController {
         model.addAttribute("mangers",userService.findAll());
 
         return "/project/create";
+    }
+    @PostMapping("/create")
+    public String insertProject(ProjectDTO project){
+
+        projectService.save(project);
+
+        return "redirect:/project/create";
     }
 }
