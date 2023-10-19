97815c5cb7126c62c91c17960a21a46a38264472
yoonisabdurahman <yoonis215@gmail.com>
2023-10-19 00:43:19 -0700

    create Task project path with classes

---
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -0,0 +1,18 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Component
+@RequestMapping("/task")
+public class TaskController {
+
+    @GetMapping("/create")
+    public String createTask(Model model){
+
+        return "task/create";
+    }
+
+}
