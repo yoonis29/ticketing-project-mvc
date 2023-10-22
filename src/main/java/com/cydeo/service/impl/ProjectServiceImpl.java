37fc36682dff050da33ad1465815b3aafa574721
yoonisabdurahman <yoonis215@gmail.com>
2023-10-21 23:47:37 -0700

    Project service updated

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -1,19 +1,34 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.TaskDTO;
+import com.cydeo.dto.UserDTO;
 import com.cydeo.enums.Status;
 import com.cydeo.service.ProjectService;
+import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+
 @Service
-public class ProjectServiceImpl extends  AbstractMapService<ProjectDTO,String> implements ProjectService {
+public class ProjectServiceImpl extends AbstractMapService<ProjectDTO, String> implements ProjectService {
+
+    private final TaskService taskService;
+
+    public ProjectServiceImpl(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     @Override
     public ProjectDTO save(ProjectDTO object) {
+
         if (object.getProjectStatus() == null) {
             object.setProjectStatus(Status.OPEN);
         }
-        return super.save(object.getProjectCode(),object);
+
+        return super.save(object.getProjectCode(), object);
     }
 
     @Override
@@ -23,11 +38,12 @@ public class ProjectServiceImpl extends  AbstractMapService<ProjectDTO,String> i
 
     @Override
     public void update(ProjectDTO object) {
+
         ProjectDTO newproject = findById(object.getProjectCode());
-        if (object.getProjectStatus()== null){
+
+        if (object.getProjectStatus() == null)
             object.setProjectStatus(newproject.getProjectStatus());
-        }
-        super.update(object.getProjectCode(),object);
+        super.update(object.getProjectCode(), object);
     }
 
     @Override
@@ -43,6 +59,38 @@ public class ProjectServiceImpl extends  AbstractMapService<ProjectDTO,String> i
     @Override
     public void complete(ProjectDTO project) {
         project.setProjectStatus(Status.COMPLETE);
-        super.save(project.getProjectCode(),project);
+        super.save(project.getProjectCode(), project);
     }
+
+    @Override
+    public List<ProjectDTO> findAllNonCompletedProjects() {
+        return findAll().stream().filter(project -> !project.getProjectStatus().equals(Status.COMPLETE)).collect(Collectors.toList());
+    }
+
+    @Override
+    public List<ProjectDTO> getCountedListOfProjectDTO(UserDTO manager) {
+
+        List<ProjectDTO> projectList =
+                findAll()
+                        .stream()
+                        .filter(project -> project.getAssignedManager().equals(manager))
+                        .map( project -> {
+
+                            List<TaskDTO> taskList = taskService.findTasksByManager(manager);
+
+                            int completeTaskCounts = (int)taskList.stream().filter(t ->  t.getProject().equals(project) && t.getTaskStatus() == Status.COMPLETE).count();
+
+                            int unfinishedTaskCounts = (int)taskList.stream().filter(t ->  t.getProject().equals(project) && t.getTaskStatus() != Status.COMPLETE).count();
+
+                            project.setCompleteTaskCounts(completeTaskCounts);
+                            project.setUnfinishedTaskCounts(unfinishedTaskCounts);
+
+                            return project;
+
+                        }).collect(Collectors.toList());
+
+
+        return projectList;
+    }
+
 }
